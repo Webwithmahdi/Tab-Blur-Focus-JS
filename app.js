da8313ec855ca69c4dedc93aecc901a3518657ed
Mahdi Baghaei <webwithmahdi@gmail.com>
2023-05-20 15:56:08 +0330

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+window.addEventListener('focus',function(){
+    this.document.title='Your welcome buddy';
+})
+window.addEventListener("blur",function(){
+    this.document.title='Come back here buddyyyyy!';
+})
